7a92714f167b83e072d30c13dbad837d49684779
Isabella Moreira <69806627+bellaIde@users.noreply.github.com>
2022-03-01 21:50:00 -0300

    Add files via upload

---
+++ Par ou impar.py
@@ -0,0 +1,12 @@
+# 🚨 Não mude o código abaixo 👇
+number = int(input("Qual número você quer checar? "))
+# 🚨 Não mude o código acima 👆
+
+#Escreva seu código aqui 👇
+resto = number % 2
+
+if resto == 0 :
+    print("Isso é um número par")
+else:
+    print("Isso é um número impar")
+
